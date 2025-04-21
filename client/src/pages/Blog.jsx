ff803ab559ad02e51ca26091c98d2866890829a4
Saqib-17 <sakib17shahidul@gmail.com>
2025-04-22 05:17:00 +0600

    ready to deploy

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import '../styles/Blog.css'; // Make sure this path is correct
 
 const Blog = () => {
   return (
-    <div className="text-center text-2xl font-bold text-dark-3">
-      Blog page coming soon!
+    <div className="blog-container">
+      <h1 className="blog-message">Blog page coming soon!</h1>
     </div>
   );
 };
